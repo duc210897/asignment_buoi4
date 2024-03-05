60a53d5642f1080e40bef1044433d24e4c5c3ca5
duc210897 <minhduc09777@gmail.com>
2024-03-05 08:16:35 +0700

    Complete task

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include "stdio.h"
 #include "conio.h"
 
-/* T.B.D: There are some warning, will be improve later */
-
 enum e_type // prefix e_ <= enum
 {
     TYPE_PRODUCT_CLOTHING,
@@ -25,7 +23,7 @@ struct st_product_information // prefix st_ <= struct
     char name[10];
 } typedef product_information_t;
 
-product_information_t g_product_available[100] =  // limit 100 product
+product_information_t g_product_available[100] =  // limit 100 product, improve for using the heap íntead
 {
    // example:
    // [0] = {.name = "T-shirt", .price = 15.99, .type = TYPE_PRODUCT_CLOTHING, .weight_or_size.size = 2}, 
@@ -35,16 +33,19 @@ product_information_t g_product_available[100] =  // limit 100 product
 char *gp_product_array_display[] = 
 {
     "Enter the number of products: ",
-    "\nEnter information for product",
-    "\nName:",
-    "Price:",
-    "Type:",
-    "Size:",
-    "Weight:",
-    "\nEnter the name of the product to retrieve information",
+    "\nEnter information for product ",
+    "\nName: ",
+    "Price: ",
+    "Type (0: Clothing, 1: Footwear, 2: Electronics): ",
+    "Size: ",
+    "Weight: ",
+    "\nEnter the name of the product to retrieve information: ",
+    "Product Information:\n",
+    "\nType: "
 };
 
 char *type_of_input[] = {"%s", "%f", "%d", "%d", "%f"};
+char *type_of_output[] = {"%s", "%2f", "%d", "%d", "%2f"};
 
 char size_or_weight_of_input_lut[] = 
 {
@@ -52,25 +53,30 @@ char size_or_weight_of_input_lut[] =
     [TYPE_PRODUCT_FOOTWEAR] = 0,
     [TYPE_PRODUCT_ELECTRONIC] = 1,
 };
-int main(int argc, char *argv[])
+char *size_or_weight_of_output_lut[] = 
 {
+    [TYPE_PRODUCT_CLOTHING] = "Clothing",
+    [TYPE_PRODUCT_FOOTWEAR] = "Footwear",
+    [TYPE_PRODUCT_ELECTRONIC] = "Electronics",
+};
 
+void user_input_product_data()
+{
   printf("%s", gp_product_array_display[0]);
   int count;
   scanf("%d", &count);
   for (int input_infor_count = 1; input_infor_count < count + 1; input_infor_count++)
   {
-    printf("%s ", gp_product_array_display[input_infor_count]);
+    printf("%s ", gp_product_array_display[1]);
     printf("%d:", input_infor_count);
 
-    /* T.B.D: There is many warning right here but improve later */
     int product_count = input_infor_count - 1;
     int *p_product_item_adress[] = {
-        [0] = &g_product_available[product_count].name, 
-        [1] = &g_product_available[product_count].price, 
-        [2] = &g_product_available[product_count].type,
-        [3] = &g_product_available[product_count].weight_or_size.size,
-        [4] = &g_product_available[product_count].weight_or_size.weight,
+        [0] = (int *) &g_product_available[product_count].name, 
+        [1] = (int *) &g_product_available[product_count].price, 
+        [2] = (int *) &g_product_available[product_count].type,
+        [3] = (int *) &g_product_available[product_count].weight_or_size.size,
+        [4] = (int *) &g_product_available[product_count].weight_or_size.weight,
     };
 
     for(int input_product_item_count = 2;  input_product_item_count < 5; input_product_item_count++)
@@ -83,17 +89,66 @@ int main(int argc, char *argv[])
     printf("%s", gp_product_array_display[5 + offset]);
     scanf(type_of_input[3 + offset], p_product_item_adress[3 + offset]);
   }
+}
 
-  /* show value*/
-//   for (int input_infor_count = 1; input_infor_count < count + 1; input_infor_count++)
-//   {
-//      printf("%s\n", g_product_available[input_infor_count].name);
-//      printf("%f\n", g_product_available[input_infor_count].price);
-//      printf("%d\n", g_product_available[input_infor_count].type);
-//      printf("%d\n", g_product_available[input_infor_count].weight_or_size.size);
-//   }
+int compare_product_data(char str[])
+{
+    for(int i = 0; i < 100; i++) // count the product available, return the index
+    {
+        for(char namecount = 0; namecount < 10; namecount++)
+        {
+            if(g_product_available[i].name[namecount] != str[namecount])
+            {
+                break;
+            }
+            if(namecount == 9)
+            {
+                return i;
+            }
+        }
+    }
 
+    return -1;
+}
 
+void user_search_product_data()
+{
+    printf("%s", gp_product_array_display[7]);
+    char search_product[100] = {0};
+    scanf("%s", search_product);
+
+    int product_count = compare_product_data(search_product);
+    
+    if (product_count == -1) return;
+
+    printf("%s", gp_product_array_display[8]);
+    printf("%s", gp_product_array_display[2]);
+    printf(type_of_output[0], g_product_available[product_count].name);
+    printf("\n");
+
+    printf("%s", gp_product_array_display[3]);
+    printf(type_of_output[1], g_product_available[product_count].price);
+
+    printf("%s", gp_product_array_display[9]);
+    printf("%s", size_or_weight_of_output_lut[g_product_available[product_count].type]);
+    printf("\n");
+
+    int offset = size_or_weight_of_input_lut[g_product_available[product_count].type];
+    printf("%s", gp_product_array_display[5 + offset]);
+    if(offset == 1)
+    {
+      printf(type_of_output[3 + offset], g_product_available[product_count].weight_or_size.weight);
+    }
+    else
+    {
+      printf(type_of_output[3 + offset], g_product_available[product_count].weight_or_size.size);
+    }
+}
+int main(int argc, char *argv[])
+{
+
+  user_input_product_data();
+  user_search_product_data();
   return 0;
 
 }
